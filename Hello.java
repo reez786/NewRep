c1c8eca397287032f9ed46377d97d20afcfa3176
Reez <rishukhan.khan3@gmail.com>
2022-03-01 16:37:53 -0600

    some changes added

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-             System.out.println("adding one more lines by Reez") ; 
+             System.out.println("adding one more lines by Reez");
+	     System.out.println("making some new changes ");
 	    System.out.println("Hello, World!"); 
     }
 }
