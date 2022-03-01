17f40ae9e849ca2e5878eaaa0767d93441deac3b
Reez <rishukhan.khan3@gmail.com>
2022-03-01 16:42:57 -0600

    Bugs fixed

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class HelloWorld {
     public static void main(String[] args) {
              System.out.println("adding one more lines by Reez");
 	     System.out.println("making some new changes ");
+	     System.out.println*("last changes done");
 	    System.out.println("Hello, World!"); 
     }
 }
